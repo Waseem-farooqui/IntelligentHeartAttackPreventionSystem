31475f460c87c696157e973c7f9a6f7d902c8c4f
waseem <waseem.farooqui@infosecurity.com.pk>
2018-01-06 01:24:48 +0500

    Initial commit

---
+++ Registeration/src/main/java/com/waseem/intelligentheartattackpreventionsystem/registeration/HttpMethodsCall.java
@@ -0,0 +1,30 @@
+package com.waseem.intelligentheartattackpreventionsystem.registeration;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class HttpMethodsCall extends Activity {
+	TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_methods_call);
+		tv=(TextView)findViewById(R.id.textfor);
+		GetMethod test=new GetMethod();
+		String returned;
+		try {
+			returned=test.GetInternetData();
+			tv.setText(returned);
+		} catch (Exception e) {
+			Toast.makeText(HttpMethodsCall.this,e.getMessage(),Toast.LENGTH_LONG).show();
+		}
+    }
+
+
+}
